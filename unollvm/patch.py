e8f6e578ab638cabf0975c7f8737336da172e115
blukat29 <yunjong@kaist.ac.kr>
2017-03-20 10:59:25 +0900

    exec_block

--- unollvm/patch.py
+++ unollvm/patch.py
@@ -4,6 +4,10 @@ import claripy
 import keystone
 import re
 
+def sym_get_val(sym):
+    assert sym.op == 'BVV'
+    return sym.args[0]
+
 class PatchAnalysis:
 
     def __init__(self, proj, shape, dispatch):
@@ -12,8 +16,52 @@ class PatchAnalysis:
         self.dispatch = dispatch
         self.ks = keystone.Ks(keystone.KS_ARCH_X86, keystone.KS_MODE_64)
 
+    def orig_state(self, case):
+        state = self.proj.factory.blank_state(addr=case)
+        state.regs.rbp = 0x100000
+        return state
+
+    def state_get_sv(self, state):
+        return state.memory.load(self.dispatch.state_var_loc, 4).reversed
+
+    def single_step(self, state, pc):
+        state.regs.pc = pc
+        path = self.proj.factory.path(state)
+        path.step(num_inst=1)
+        assert len(path.successors) == 1
+        return path.successors[0].state
+
+    def exec_instrs(self, instruction_addrs, state):
+        for addr in instruction_addrs:
+            state = self.single_step(state, addr)
+        return state
+
+    def exec_block(self, addr, state):
+        block = self.proj.factory.block(addr)
+        jk = block.vex.jumpkind
+        if jk == 'Ijk_Boring':
+            state = self.exec_instrs(block.instruction_addrs, state)
+            # Assume we do not meet conditional branch.
+            return sym_get_val(state.regs.pc), state
+        elif jk == 'Ijk_Call':
+            state = self.exec_instrs(block.instruction_addrs[:-1], state)
+            return addr + block.size, state
+        else:
+            raise Exception('cannot handle jumpkind "%s"' % jk)
+
+    def analyze_case(self, case):
+        print '------ %8x' % case
+
+        state = self.orig_state(case)
+        orig_sv = self.state_get_sv(state)
+
+        addr = case
+        while addr != self.shape.collector_addr and (addr not in self.shape.leaf_addrs):
+            addr, state = self.exec_block(addr, state)
+            print hex(addr)
+
     def patches(self):
         p = []
         for case in self.dispatch.block_map.values():
-            break
+            self.analyze_case(case)
         return p
