09bdf1a9d8c6133490f246c9f30e94c9bfc47dd3
blukat29 <yunjong@kaist.ac.kr>
2017-09-26 03:55:20 +0900

    main.py takes arguments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import unollvm
 import sys
 
-filename = 'example/small.fla'
-addr = 0x400480
+filename = sys.argv[1]
+addr = int(sys.argv[2], 16)
 do = unollvm.Deobfuscator(filename)
 do.analyze_func(addr)
