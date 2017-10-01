593ab3e2395cc5e375b8ba5ccc168d34e8aea99d
Yunjong Jeong <yunjong@kaist.ac.kr>
2017-10-02 01:45:08 +0900

    Report progress

--- unollvm/deobfus.py
+++ unollvm/deobfus.py
@@ -22,42 +22,53 @@ class Deobfuscator(object):
         self.cfg_cache = None
         self.patches = {}
 
-    def log(self, message):
-        if self.verbose:
+    def log(self, message, always=False):
+        if self.verbose or always:
             self.logfile.write(message)
-            self.logfile.write('\n')
+            self.logfile.flush()
 
     def cfg(self):
         if self.cfg_cache is None:
             self.cfg_cache = self.proj.analyses.CFGFast()
         return self.cfg_cache
 
-    def analyze_addr(self, addr):
-        self.analyze_func(addr)
-
-    def analyze_name(self, name):
-        symbol = self.proj.loader.main_object.get_symbol(name)
-        self.analyze_func(symbol.linked_addr)
-
-    def analyze_func(self, addr):
-        func = self.cfg().functions[addr]
-        self.log('Analysis for {}'.format(repr(func)))
-
+    def analyze_func(self, func):
         shape = Shape(func)
         self.log(shape.dump())
         if not shape.is_ollvm:
-            return
+            return False
+        self.log('\n')
 
         control = Control(self.proj, shape)
-        self.log(control.dump())
+        self.log(control.dump() + '\n')
 
         patch = Patch(self.proj, shape, control, self.ks)
-        self.log(patch.dump())
+        self.log(patch.dump() + '\n')
+
         self.patches.update(patch.patches)
+        return True
+
+    def analyze_addr(self, addr):
+        func = self.cfg().functions[addr]
+        self.log('\n')
+        self.log('Patching {} ...'.format(repr(func)), True)
+        self.log('\n')
+
+        if func.is_syscall: self.log(' skip (syscall).\n', True)
+        elif func.is_plt: self.log(' skip (plt).\n', True)
+        elif func.is_simprocedure: self.log(' skip (simprocedure).\n', True)
+        else:
+            success = self.analyze_func(func)
+            if success: self.log(' done.\n', True)
+            else: self.log(' fail.\n', True)
+
+    def analyze_name(self, name):
+        symbol = self.proj.loader.main_object.get_symbol(name)
+        self.analyze_addr(symbol.linked_addr)
 
     def analyze_all(self):
         for addr in self.cfg().functions:
-            self.analyze_func(addr)
+            self.analyze_addr(addr)
 
     def commit(self, output):
         patch_elf(self.filename, output, self.patches)
