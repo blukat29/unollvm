861e431021459efbdbb676345157e1b769b747d9
blukat29 <yunjong@kaist.ac.kr>
2017-10-28 20:52:21 +0900

    Produce multiple versions of examples

--- example/small.c
+++ example/small.c
@@ -1,12 +1,11 @@
 int go(int n)
 {
-    if (n % 3 == 0) return n + 1;
-    else if (n % 3 == 1) return n * 2;
+    if (n == 0) return n + 1;
+    else if (n == 1) return n * 2;
     else return n * n;
 }
 
 int main(int argc, char** argv)
 {
-    go(argc);
-    return 0;
+    return go(argc);
 }
